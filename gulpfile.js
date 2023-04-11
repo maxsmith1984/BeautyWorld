d042bff633be97a810feae8f7d35f0b9baece766
Maksim Salchak <killingtime2101@mail.ru>
2023-04-11 17:23:44 +0700

    изменен путь стилей галп файл

--- gulpfile.js
+++ gulpfile.js
@@ -86,5 +86,5 @@ function buildJs() {
 exports.images = images;
 exports.cleanimg = cleanimg;
 
-exports.build = series(cleandist, parallel(styles, images, buildcopy, buildJs));
+exports.build = series(cleandist, parallel(buildcopy, styles, images, buildJs));
 exports.default = series([styles, buildJs], parallel(browsersync, startwatch));
