54fdaea425a990b1425b8c4eed7b500c77895bb3
Maksim Salchak <killingtime2101@mail.ru>
2023-04-11 17:34:05 +0700

    изменен конфиг вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    output: {
-        filename: 'bundle.js',
-    },
     mode: process.env.NODE_ENV,
     module: {
         rules: [
