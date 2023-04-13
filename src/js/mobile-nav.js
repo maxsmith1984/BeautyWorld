397e9d03bab21c7735ce47e8b1b1d577ae2dec47
Maksim Salchak <killingtime2101@mail.ru>
2023-04-13 20:30:01 +0700

    изменен конфиг webpack

---
+++ src/js/mobile-nav.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    const navBtn = document.querySelector('#burger');
+    const mobileMenu = document.querySelector('#mobile-menu');
+
+    const toggleMenu = (() => {
+        navBtn.classList.toggle('burger__transform');
+        mobileMenu.classList.toggle('mobile-nav_show');
+    });
+    const closeMenu = (() => {
+        navBtn.classList.remove('burger__transform');
+        mobileMenu.classList.remove('mobile-nav_show');
+    });
+
+    navBtn.addEventListener('click', toggleMenu);
+    document.addEventListener('scroll', closeMenu);
+
+
+    const smoothScrollLinks = document.querySelectorAll('.navigation__link');
+
+    for (let link of smoothScrollLinks) {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const target = event.target;
+            const elementToScroll = document.querySelector(target.getAttribute('href'));
+            elementToScroll.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            closeMenu();
+        });
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     mode: process.env.NODE_ENV,
+    devtool: isProduction ? false : 'inline-source-map',
     module: {
         rules: [
             {
