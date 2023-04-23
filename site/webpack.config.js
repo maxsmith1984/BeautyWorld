b503048add6ca649ac82ea65df42f8328c658b4c
Maksim Salchak <killingtime2101@mail.ru>
2023-04-23 11:07:26 +0700

    добавлен TerserPlugin в сборку

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
@@ -6,6 +7,22 @@ module.exports = {
         hints: false,
         maxEntrypointSize: 512000,
         maxAssetSize: 512000
+    },
+    optimization: {
+        minimize: isProduction,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: isProduction,
+                    },
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
     },
     mode: process.env.NODE_ENV,
     devtool: isProduction ? false : 'inline-source-map',
