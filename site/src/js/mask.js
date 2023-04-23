428fe38246aa3675b0ae9360fe33d27371de5bbd
Maksim Salchak <killingtime2101@mail.ru>
2023-04-23 11:45:42 +0700

    отредактирована маска номера

--- site/src/js/mask.js
+++ site/src/js/mask.js
@@ -1,3 +1,10 @@
 import Inputmask from "inputmask";
 
-Inputmask({ mask: "+7 (999) 999-99-99" }).mask(document.querySelectorAll('#phone'));
+Inputmask({
+    mask: "+7 (999) 999-99-99",
+    autoUnmask: true,
+    clearIncomplete: true,
+}).mask(document.querySelectorAll('#phone'));
+
+
+
